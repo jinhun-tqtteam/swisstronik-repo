957a0aadd4a76129a8ae83975e3d943c1e7637d3
tqtien <tqtien@hanatourjsv.com>
2024-09-06 11:46:26 +0700

    update source

--- scripts/proxy/utils/deployed-address.ts
+++ scripts/proxy/utils/deployed-address.ts
@@ -1,4 +1,4 @@
-export const SWTRProxy = '0xaCF84CED4eedD053C0417082F09bf2bB2d45899E'
-export const ProxyAdmin = '0xC72182487A5D82aD21F26B02900b9B7265442355'
-export const SWTRImplementation = '0xE75eD02BA12089212471dB9CB22AdFa577ea2Af5'
+export const SWTRProxy = '0x3B54591D498bce06bc827eBa34421077bD1aD111'
+export const ProxyAdmin = '0x89a93eb708e4916e04D3C960C88444D9af3CCb06'
+export const SWTRImplementation = '0x2565d6f6d0DB0903F48da553280E944592b15fe9'
     
